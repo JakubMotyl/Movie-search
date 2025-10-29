4193afe3976dd35427a63bfc870b5839eb8434bd
Jakub Motyl <kubamotyl16@gmail.com>
2025-10-29 09:09:04 +0100

    Repair add/remove buttons

--- src/components/Home/Search.jsx
+++ src/components/Home/Search.jsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { API_KEY, BASE_URL } from '../../scripts/api';
+import React, { useEffect, useState } from "react";
+import { API_KEY, BASE_URL } from "../../scripts/api";
 
 export default function Search({ setFavorites }) {
   const [movies, setMovies] = useState([]);
-  const [activeCategory, setActiveCategory] = useState('popular');
-  const [query, setQuery] = useState('')
-  const [popupMessage, setPopupMessage] = useState('');
+  const [activeCategory, setActiveCategory] = useState("popular");
+  const [query, setQuery] = useState("");
+  const [popupMessage, setPopupMessage] = useState("");
   const [showPopup, setShowPopup] = useState(false);
 
   const moviesCategories = [
-    {id: 'popular', name: 'Popular'},
-    {id: 'top_rated', name: 'Top Rated'},
-    {id: 28, name: 'Action'},
-    {id: 35, name: 'Comedy'},
-    {id: 27, name: 'Horror'},
-    {id: 10749, name: 'Romance'},
-    {id: 878, name: 'Science Fiction'},
-    {id: 16, name: 'Animation'}
-  ]
+    { id: "popular", name: "Popular" },
+    { id: "top_rated", name: "Top Rated" },
+    { id: 28, name: "Action" },
+    { id: 35, name: "Comedy" },
+    { id: 27, name: "Horror" },
+    { id: 10749, name: "Romance" },
+    { id: 878, name: "Science Fiction" },
+    { id: 16, name: "Animation" },
+  ];
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let url = '';
+        let url = "";
 
         if (query) {
-          url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${encodeURIComponent(query)}`
+          url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${encodeURIComponent(
+            query
+          )}`;
         } else {
-          if (activeCategory === 'popular') {
-          url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US`;
-          } else if (activeCategory === 'top_rated'){
+          if (activeCategory === "popular") {
+            url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US`;
+          } else if (activeCategory === "top_rated") {
             url = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`;
           } else {
             url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${activeCategory}&language=en-US`;
@@ -39,33 +41,35 @@ export default function Search({ setFavorites }) {
         const data = await res.json();
         // console.log(data.results)
         setMovies(data.results.slice(0, columns * 2));
-        
       } catch (error) {
-        console.log('Error fetching data', error)
-      } 
-    }
+        console.log("Error fetching data", error);
+      }
+    };
 
     const delayDebounce = setTimeout(() => {
       fetchData();
     }, 300);
 
-    return () => clearTimeout(delayDebounce)
-
-  }, [activeCategory, query])
+    return () => clearTimeout(delayDebounce);
+  }, [activeCategory, query]);
 
   // Always 12 Movies
-  const columns = window.innerWidth >= 1024 ? 6
-    : window.innerWidth >= 768 ? 4
-    : window.innerWidth >= 640 ? 3
-    : 2;
+  const columns =
+    window.innerWidth >= 1024
+      ? 6
+      : window.innerWidth >= 768
+      ? 4
+      : window.innerWidth >= 640
+      ? 3
+      : 2;
 
   const handleAddToFavorites = (movie) => {
-    setFavorites(prev => {
-      if (prev.some(fav => fav.id === movie.id)) return prev;
+    setFavorites((prev) => {
+      if (prev.some((fav) => fav.id === movie.id)) return prev;
       // console.log([...prev, movie])
       return [...prev, movie];
-    })
-    
+    });
+
     // Popup message
     setPopupMessage(`${movie.title}\nhas been added to your list.`);
     setShowPopup(true);
@@ -73,81 +77,85 @@ export default function Search({ setFavorites }) {
     setTimeout(() => {
       setShowPopup(false);
     }, 2000);
-
-  }
+  };
 
   return (
-    <div className='p-10 bg-black text-white flex flex-col gap-10'>
-      <div className='text-center md:text-5xl text-4xl font-extralight'>
+    <div className="p-10 bg-black text-white flex flex-col gap-10">
+      <div className="text-center md:text-5xl text-4xl font-extralight">
         <p>Ready to Chill?</p>
       </div>
-      <div className='flex items-center justify-center'>
+      <div className="flex items-center justify-center">
         <input
-          className='input-search placeholder:text-sm placeholder:md:text-[1rem]'  
-          placeholder='Type your movies titles here...'
-          type="text" 
+          className="input-search placeholder:text-sm placeholder:md:text-[1rem]"
+          placeholder="Type your movies titles here..."
+          type="text"
           value={query}
-          onChange={e => setQuery(e.target.value)}
+          onChange={(e) => setQuery(e.target.value)}
         />
       </div>
       {/* MOVIES CATEGORIES */}
-      <div className='flex gap-3 flex-wrap'>
+      <div className="flex gap-3 flex-wrap">
         {moviesCategories.map(({ id, name }) => {
           return (
-            <div 
+            <div
               className={`px-4 py-2 rounded-2xl duration-300
-                ${activeCategory  === id ? "bg-red-600 hover:bg-red-500" : "bg-gray-800 hover:bg-gray-700"}`} 
+                ${
+                  activeCategory === id
+                    ? "bg-red-600 hover:bg-red-500"
+                    : "bg-gray-800 hover:bg-gray-700"
+                }`}
               key={id}
             >
-              <button className='cursor-pointer' 
+              <button
+                className="cursor-pointer"
                 onClick={() => {
-                  setActiveCategory(id)
-                  setQuery('')
+                  setActiveCategory(id);
+                  setQuery("");
                 }}
               >
                 {name}
               </button>
             </div>
-          )
+          );
         })}
       </div>
       {/* MOVIES GRID */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {movies
-          .filter(movie => movie.poster_path)
-          .map(movie => (
-            <div key={movie.id} className="bg-gray-800 hover:bg-gray-700 relative
-              rounded-lg overflow-hidden hover:scale-[1.03] duration-300 group"
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div
+              key={movie.id}
+              className="bg-gray-800 hover:bg-gray-700 relative
+              rounded-lg overflow-hidden hover:scale-[1.03] duration-300"
             >
-              <img 
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-                alt={movie.title} 
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
                 className="w-full h-auto"
               />
-              <div className='absolute top-1 right-1 p-2 text-sm bg-gray-800 rounded-full opacity-0
-              hover:bg-gray-700 duration-300 group-hover:opacity-100'
-              >
-                <button onClick={() => handleAddToFavorites(movie)} className='cursor-pointer'>
+              <div className="absolute top-1 right-1 p-2 text-sm rounded-full bg-gray-800 hover:bg-gray-700">
+                <button
+                  onClick={() => handleAddToFavorites(movie)}
+                  className="cursor-pointer"
+                >
                   <i className="fa-solid fa-bookmark"></i>
                 </button>
               </div>
             </div>
-        ))}
+          ))}
       </div>
       {/* SHOW MORE */}
-      <div className='flex items-center justify-center'>
-        <button className='show-more-btn'
-        >+</button>
+      <div className="flex items-center justify-center">
+        <button className="show-more-btn">+</button>
       </div>
 
       {/* Popup Message */}
-      {
-        showPopup && (
-          <div className='fixed bottom-5 right-5 whitespace-pre-line text-white rounded-lg text-[0.85rem] font-light bg-green-600 px-4 py-2 opacity-0 animate-[fadeIn_0.3s_ease-out_forwards] popup-shadow'>
-            {popupMessage}
-          </div>
-        )
-      }
+      {showPopup && (
+        <div className="fixed bottom-5 right-5 whitespace-pre-line text-white rounded-lg text-[0.85rem] font-light bg-green-600 px-4 py-2 opacity-0 animate-[fadeIn_0.3s_ease-out_forwards] popup-shadow">
+          {popupMessage}
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- src/components/MyList/Hero.jsx
+++ src/components/MyList/Hero.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export default function Hero({ favorites, setFavorites }) {
-  const [popupMessage, setPopupMessage] = useState('');
+  const [popupMessage, setPopupMessage] = useState("");
   const [showPopup, setShowPopup] = useState(false);
 
   const handleRemoveFavorites = (movie) => {
-    setFavorites(favorites.filter(item => item.id !== movie.id));
+    setFavorites(favorites.filter((item) => item.id !== movie.id));
 
     // Popup message
     setPopupMessage(`${movie.title}\nhas been removed from your list.`);
@@ -14,7 +14,7 @@ export default function Hero({ favorites, setFavorites }) {
     setTimeout(() => {
       setShowPopup(false);
     }, 2000);
-  }
+  };
 
   return (
     <div className="min-h-[100vh] px-6 md:px-12 py-20 bg-black text-white flex flex-col gap-10">
@@ -34,8 +34,8 @@ export default function Hero({ favorites, setFavorites }) {
           {favorites.map((favorite, key) => (
             <div
               key={key}
-              className="bg-gray-800 hover:bg-gray-700 relative rounded-xl overflow-hidden 
-                hover:scale-105 duration-300 shadow-md group"
+              className="bg-gray-800 relative rounded-xl overflow-hidden 
+                hover:scale-105 duration-300 shadow-md"
             >
               <img
                 src={`https://image.tmdb.org/t/p/w500${favorite.poster_path}`}
@@ -44,13 +44,12 @@ export default function Hero({ favorites, setFavorites }) {
               />
               {/* Title */}
               <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/80 to-transparent p-3">
-                <h2 className="text-sm md:text-base font-medium">{favorite.title}</h2>
+                <h2 className="text-sm md:text-base font-medium">
+                  {favorite.title}
+                </h2>
               </div>
               {/* Remove button */}
-              <div
-                className="absolute top-1 right-1 p-2 text-sm bg-black/70 rounded-full opacity-0 
-                  group-hover:opacity-100 hover:bg-red-600 duration-300"
-              >
+              <div className="absolute top-1 right-1 p-2 text-sm rounded-full bg-red-600 hover:bg-red-500">
                 <button
                   onClick={() => handleRemoveFavorites(favorite)}
                   className="cursor-pointer"
@@ -68,14 +67,11 @@ export default function Hero({ favorites, setFavorites }) {
       )}
 
       {/* Popup Message */}
-      {
-        showPopup && (
-          <div className='fixed bottom-5 right-5 whitespace-pre-line text-white rounded-lg text-[0.85rem] font-light bg-red-600 px-4 py-2 opacity-0 animate-[fadeIn_0.3s_ease-out_forwards] popup-shadow'>
-            {popupMessage}
-          </div>
-        )
-      }
+      {showPopup && (
+        <div className="fixed bottom-5 right-5 whitespace-pre-line text-white rounded-lg text-[0.85rem] font-light bg-red-600 px-4 py-2 opacity-0 animate-[fadeIn_0.3s_ease-out_forwards] popup-shadow">
+          {popupMessage}
+        </div>
+      )}
     </div>
-    
-  )
+  );
 }
